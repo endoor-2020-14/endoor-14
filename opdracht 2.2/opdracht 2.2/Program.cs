bfe20757a73fadac8d25c8c263bb869b67f3f38b
endoor-2020-14 <62386977+endoor-2020-14@users.noreply.github.com>
2020-04-12 16:55:13 +0200

    opdracht2_2

---
+++ opdracht 2.2/opdracht 2.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opdracht_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // vraag de gebruiker om zijn gewicht en lengte to typen
+
+            Console.Write("Mag u gewicht in Kg typen alstublieft: ");
+            double gewicht = Double.Parse(Console.ReadLine());
+            gewicht = Math.Round(gewicht, 1);
+
+            Console.Write("Mag u lengte in cm invoeren: ");
+            double lengte = double.Parse(Console.ReadLine());
+
+            //berekenen BMI
+
+            double BMI = gewicht / (lengte * lengte);
+            BMI = Math.Round(BMI, 1);
+            //print BMI
+
+            Console.WriteLine("Jou BMI is = " + BMI);
+
+        }
+    }
+}
